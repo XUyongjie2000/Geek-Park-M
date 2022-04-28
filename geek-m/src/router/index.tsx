9b7fcaef93c2204ac599f95a94fe1d58d004041a
胥永杰 <99706807@qq.com>
2022-04-28 09:45:57 +0800

    重定向首页内容

--- geek-m/src/router/index.tsx
+++ geek-m/src/router/index.tsx
@@ -1,8 +1,12 @@
-import { useRoutes } from "react-router";
+import { Navigate, useRoutes } from "react-router";
 import Layout from "../pages/Layout";
 import Login from "../pages/Login";
 
 const routes = [
+  {
+    path: "/",
+    element: <Navigate to="/home" />,
+  },
   {
     path: "/home",
     element: <Layout />,
